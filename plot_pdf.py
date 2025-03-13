27ebd015a869c5d3ee293cd0db0f6d0edd6fd97b
robjmcgibbon <robjmcgibbon@gmail.com>
2025-03-13 15:12:12 +0000

    Multiple properties on PDF plots

--- plot_pdf.py
+++ plot_pdf.py
@@ -6,6 +6,7 @@ import argparse
 import os
 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import LogLocator
 import numpy as np
 import unyt
 import swiftsimio as sw
@@ -65,70 +66,153 @@ def load_agn_gas_densities(snap):
 # define load_dataset, bins, xlabel, ylabelk
 prop_info =  {
     'birth_densities': (
-        load_stellar_birth_densities,
-        unyt.unyt_array(np.logspace(-2, 7, number_of_bins), units="1/cm**3"),
+        [
+            (
+                load_stellar_birth_densities,
+                unyt.unyt_array(np.logspace(-2, 7, number_of_bins), units="1/cm**3"),
+                "-",
+                "",
+            ),
+        ],
         "Birth density $n_{\\rm H}$ [cm$^{-3}$]",
         "$n_{\\rm bin}$ / d$\\log_{10}n_{\\rm H}$ / $n_{\\rm total}$",
     ),
     'birth_temperatures': (
-        load_stellar_birth_temperatures,
-        unyt.unyt_array(np.logspace(1, 4.5, number_of_bins), units="K"),
+        [
+            (
+                load_stellar_birth_temperatures,
+                unyt.unyt_array(np.logspace(1, 4.5, number_of_bins), units="K"),
+                "-",
+                "",
+            ),
+        ],
         "Birth temperature $T_b$ [k]",
         "$n_{\\rm bin}$ / d$\\log_{10}T_b$ / $n_{\\rm total}$",
     ),
     'birth_velocity_dispersions': (
-        load_stellar_birth_velocity_dispersions,
-        unyt.unyt_array(np.logspace(0, 2.5, number_of_bins), units="km/s"),
+        [
+            (
+                load_stellar_birth_velocity_dispersions,
+                unyt.unyt_array(np.logspace(0, 2.5, number_of_bins), units="km/s"),
+                "-",
+                "",
+            ),
+        ],
         "Birth velocity dispersion $\\sigma{}_b$ [km s$^{-1}$]",
         "$n_{\\rm bin}$ / d$\\log_{10}\\sigma{}_b$ / $n_{\\rm total}$",
     ),
     'densities_at_last_supernova_event': (
-        load_snii_gas_densities,
-        unyt.unyt_array(np.logspace(-5, 7, number_of_bins), units="1/cm**3"),
+        [
+            (
+                load_snii_gas_densities,
+                unyt.unyt_array(np.logspace(-5, 7, number_of_bins), units="1/cm**3"),
+                "-",
+                "",
+            ),
+        ],
         "Density of the gas heated by CCSN $\\rho_{\\rm CCSN}$ [$n_{\\rm H}$ cm$^{-3}$]",
         "$n_{\\rm bin}$ / d$\\log_{10}\\rho_{\\rm CCSN}$ / $n_{\\rm total}$",
     ),
     'densities_at_last_agn_event': (
-        load_agn_gas_densities,
-        unyt.unyt_array(np.logspace(-5, 7, number_of_bins), units="1/cm**3"),
+        [
+            (
+                load_agn_gas_densities,
+                unyt.unyt_array(np.logspace(-5, 7, number_of_bins), units="1/cm**3"),
+                "-",
+                "",
+            ),
+        ],
         "Density of the gas heated by AGN $\\rho_{\\rm AGN}$ [$n_{\\rm H}$ cm$^{-3}$]",
         "$n_{\\rm bin}$ / d$\\log_{10}\\rho_{\\rm AGN}$ / $n_{\\rm total}$",
     ),
+    'birth_ccsn_densities': (
+        [
+            (
+                load_stellar_birth_densities,
+                unyt.unyt_array(np.logspace(-2, 7, number_of_bins), units="1/cm**3"),
+                "-",
+                "Stellar birth",
+            ),
+            (
+                load_snii_gas_densities,
+                unyt.unyt_array(np.logspace(-5, 7, number_of_bins), units="1/cm**3"),
+                "--",
+                "CCSN feedback",
+            ),
+        ],
+        "Density $n_{\\rm H}$ [cm$^{-3}$]",
+        "$n_{\\rm bin}$ / d$\\log_{10}n_{\\rm H}$ / $n_{\\rm total}$",
+    ),
+    'birth_ccsn_agn_densities': (
+        [
+            (
+                load_stellar_birth_densities,
+                unyt.unyt_array(np.logspace(-2, 7, number_of_bins), units="1/cm**3"),
+                "-",
+                "Stellar birth",
+            ),
+            (
+                load_snii_gas_densities,
+                unyt.unyt_array(np.logspace(-5, 7, number_of_bins), units="1/cm**3"),
+                "--",
+                "CCSN feedback",
+            ),
+            (
+                load_agn_gas_densities,
+                unyt.unyt_array(np.logspace(-5, 7, number_of_bins), units="1/cm**3"),
+                ":",
+                "AGN feedback",
+            ),
+        ],
+        "Density $n_{\\rm H}$ [cm$^{-3}$]",
+        "$n_{\\rm bin}$ / d$\\log_{10}n_{\\rm H}$ / $n_{\\rm total}$",
+    ),
+
 }
 
-
-for name, (load_prop, bins, xlabel, ylabel) in prop_info.items():
+snap_data = {}
+for name, (to_plot, xlabel, ylabel) in prop_info.items():
     print(f'Loading and plotting {name}')
-
-    log_bin_width = np.log10(bins[1].value) - np.log10(bins[0].value)
-    centres = 0.5 * (bins[1:] + bins[:-1])
 
     fig, ax = plt.subplots(1, figsize=(5, 4), constrained_layout=False)
     plt.subplots_adjust(left=0.15, right=0.97, top=0.97, bottom=0.12)
     ax.loglog()
-    from matplotlib.ticker import LogLocator
     ax.xaxis.set_minor_locator(LogLocator(base=10.0, subs=np.arange(2, 10) * 0.1, numticks=100))
     ax.yaxis.set_minor_locator(LogLocator(base=10.0, subs=np.arange(2, 10) * 0.1, numticks=100))
 
-    for sim in args.sims:
-        print(sim)
-        snapshot_filename = f'{base_dir}/{sim}/snapshots/colibre_0127/colibre_0127.hdf5'
-        if not os.path.exists(snapshot_filename):
-            snapshot_filename = f'{base_dir}/{sim}/snapshots/colibre_0123/colibre_0123.hdf5'
-        snap = sw.load(snapshot_filename)
-        prop = load_prop(snap)
-        n_star = prop.shape[0]
+    for i_prop, (load_prop, bins, ls, ls_label) in enumerate(to_plot):
+        log_bin_width = np.log10(bins[1].value) - np.log10(bins[0].value)
+        centres = 0.5 * (bins[1:] + bins[:-1])
 
-        H, _ = np.histogram(prop, bins=bins.value)
-        y_points = H / log_bin_width / n_star
+        for sim in args.sims:
+            print(sim)
+            if sim not in snap_data:
+                # Load z=0 data
+                snapshot_filename = f'{base_dir}/{sim}/snapshots/colibre_0127/colibre_0127.hdf5'
+                if not os.path.exists(snapshot_filename):
+                    snapshot_filename = f'{base_dir}/{sim}/snapshots/colibre_0123/colibre_0123.hdf5'
+                snap_data[sim] = sw.load(snapshot_filename)
+            snap = snap_data[sim]
 
-        label, color, ls = helpers.get_sim_plot_style(sim)
-        ax.plot(centres, y_points, label=label, color=color, ls=ls)
+            prop = load_prop(snap)
+            n_part = prop.shape[0]
+
+            H, _ = np.histogram(prop, bins=bins.value)
+            y_points = H / log_bin_width / n_part
+
+            label, color, _ = helpers.get_sim_plot_style(sim)
+            if i_prop == 0:
+                ax.plot(centres, y_points, label=label, color=color, ls=ls)
+            else:
+                ax.plot(centres, y_points, color=color, ls=ls)
+
+        if ls_label != "":
+            ax.plot(centres[0], y_points[0], color='k', ls=ls, label=ls_label)
 
     ax.legend(loc="upper right", markerfirst=False)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    ax.set_ylim(bottom=1e-3)
+    ax.set_ylim(bottom=1e-3, top=1e1)
 
     fig.savefig(f"{name}_distribution.pdf")
     plt.close()
